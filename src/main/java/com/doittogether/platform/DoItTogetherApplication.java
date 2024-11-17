850ec98dacbbb3110a06f7eb79ab7aa227e18fea
Yechan Jung <skfk286@hanmail.net>
2024-11-17 18:22:02 +0900

    최초 백엔드 프로젝트 구성 추가

---
+++ src/main/java/com/doittogether/platform/DoItTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.doittogether.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoItTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoItTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/doittogether/platform/Web11DoItTogetherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doittogether.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web11DoItTogetherBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
