1ba273dcd7b1c3d274e44b13fabfa7bb091dec56
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-22 18:48:31 +0900

    init: Personality 수정

--- src/main/java/com/doittogether/platform/domain/entity/Personality.java
+++ src/main/java/com/doittogether/platform/domain/entity/Personality.java
@@ -1,10 +1,13 @@
 package com.doittogether.platform.domain.entity;
 
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +19,10 @@ public class Personality extends BaseEntity {
     private Long personalityId;
 
     private String value;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 
     public Personality(String value) {
         this.value = value;
