6290e5b63fc24034ac189429a33cb5cd4a23556e
Yechan Jung <skfk286@hanmail.net>
2024-11-22 11:36:46 +0900

    feat : 문자열 처리용 StringUtil 클래스 정의 수정

--- src/main/java/com/doittogether/platform/infrastructure/util/StringUtil.java
+++ src/main/java/com/doittogether/platform/infrastructure/util/StringUtil.java
@@ -5,7 +5,7 @@ public class StringUtil {
     /**
      * 문자열들을 점(.)으로 연결합니다.
      *
-     * @param 문자열들 점으로 연결할 문자열들 (가변 인자)
+     * @param parts 문자열들 점으로 연결할 문자열들 (가변 인자)
      * @return 점으로 연결된 단일 문자열, 또는 입력이 없으면 빈 문자열 반환
      */
     public static String joinWithDot(String... parts) {
