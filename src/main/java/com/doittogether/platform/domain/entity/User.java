51570ed1f6ec870f5174ae56fb2cd7963e6ce9ed
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-22 21:47:21 +0900

    init: User 클래스명 @Getter추가

--- src/main/java/com/doittogether/platform/domain/entity/User.java
+++ src/main/java/com/doittogether/platform/domain/entity/User.java
@@ -4,7 +4,6 @@ import static jakarta.persistence.CascadeType.ALL;
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -16,9 +15,11 @@ import java.util.ArrayList;
 import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
 @Table(name = "user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
