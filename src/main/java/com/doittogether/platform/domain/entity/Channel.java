e7032dd754c323f975c9d1fe6f32273319a68d7d
Yechan Jung <skfk286@hanmail.net>
2024-11-23 21:57:36 +0900

    refactor : Entity 관계 부분 수정(rebase)

--- src/main/java/com/doittogether/platform/domain/entity/Channel.java
+++ src/main/java/com/doittogether/platform/domain/entity/Channel.java
@@ -1,7 +1,6 @@
 package com.doittogether.platform.domain.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
