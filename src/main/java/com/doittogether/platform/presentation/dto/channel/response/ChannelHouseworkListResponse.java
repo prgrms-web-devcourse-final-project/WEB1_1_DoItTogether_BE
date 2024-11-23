456878b04c6ee986eb53b1a31d7a8ac354573b9a
Yechan Jung <skfk286@hanmail.net>
2024-11-24 02:06:42 +0900

    refactor : 집안일 리스폰스로 매핑되게 수정

--- src/main/java/com/doittogether/platform/presentation/dto/channel/response/ChannelHouseworkListResponse.java
+++ src/main/java/com/doittogether/platform/presentation/dto/channel/response/ChannelHouseworkListResponse.java
@@ -1,7 +1,6 @@
 package com.doittogether.platform.presentation.dto.channel.response;
 
 import com.doittogether.platform.domain.entity.Channel;
-import com.doittogether.platform.domain.entity.Housework;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -12,17 +11,19 @@ import java.util.List;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Schema(description = "채널의 집안일 리스트 응답")
 @Builder
-public record ChannelHouseworkListResponse (
+public record ChannelHouseworkListResponse(
         @Schema(description = "채널 아이디")
         Long channelId,
 
         @Schema(description = "채널 내의 집안일 리스트")
-        List<Housework> houseworkList
+        List<HouseworkResponse> houseworkList
 ) {
     public static ChannelHouseworkListResponse from(Channel channel) {
         return ChannelHouseworkListResponse.builder()
                 .channelId(channel.getChannelId())
-                .houseworkList(channel.getHouseworks())
+                .houseworkList(channel.getHouseworks().stream()
+                        .map(HouseworkResponse::from)
+                        .toList())
                 .build();
     }
 }
