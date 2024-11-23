4521359365ed4c6bf11f75b2958be684fbbb3f3c
Yechan Jung <skfk286@hanmail.net>
2024-11-24 02:29:15 +0900

    refactor : 은정님 피드백 반영 - builder 패턴 적용, of 키워드 수정 등

--- src/main/java/com/doittogether/platform/presentation/dto/channel/response/ChannelRegisterResponse.java
+++ src/main/java/com/doittogether/platform/presentation/dto/channel/response/ChannelRegisterResponse.java
@@ -1,8 +1,10 @@
 package com.doittogether.platform.presentation.dto.channel.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 @Schema(description = "채널의 생성 응답")
+@Builder
 public record ChannelRegisterResponse (
         @Schema(description = "채널 아이디")
         Long channelId,
@@ -10,7 +12,10 @@ public record ChannelRegisterResponse (
         @Schema(description = "채널명")
         String name
 ) {
-    public static ChannelRegisterResponse from(Long channelId, String name) {
-        return new ChannelRegisterResponse(channelId, name);
+    public static ChannelRegisterResponse of(Long channelId, String name) {
+        return ChannelRegisterResponse.builder()
+                .channelId(channelId)
+                .name(name)
+                .build();
     }
 }
