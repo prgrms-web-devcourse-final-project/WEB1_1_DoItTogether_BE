9f6fc033e857f838e623b7fae154d1cb116b9db2
Yechan Jung <skfk286@hanmail.net>
2024-11-24 20:58:33 +0900

    refactor : 매핑 DTO 에 맞게 Controller 수정 및 Swagger 수정

--- src/main/java/com/doittogether/platform/presentation/dto/channel/response/UserResponse.java
+++ src/main/java/com/doittogether/platform/presentation/dto/channel/response/UserResponse.java
@@ -1,9 +1,13 @@
 package com.doittogether.platform.presentation.dto.channel.response;
 
 import com.doittogether.platform.domain.entity.User;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Schema(description = "회원 정보 응답")
 @Builder
 public record UserResponse(
         @Schema(description = "유저 아이디")
