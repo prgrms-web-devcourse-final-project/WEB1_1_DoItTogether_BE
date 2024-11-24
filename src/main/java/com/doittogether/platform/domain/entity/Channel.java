5bd6d1ba30db6da453ebb340ec47c7e071e31d89
Yechan Jung <skfk286@hanmail.net>
2024-11-24 20:08:59 +0900

    refactor : channel 도메인의 name field not null 정의

--- src/main/java/com/doittogether/platform/domain/entity/Channel.java
+++ src/main/java/com/doittogether/platform/domain/entity/Channel.java
@@ -1,6 +1,7 @@
 package com.doittogether.platform.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,6 +20,7 @@ public class Channel extends BaseEntity {
     @GeneratedValue(strategy = IDENTITY)
     private Long channelId;
 
+    @NotNull
     private String name;
 
     @OneToMany(mappedBy = "channel", cascade = CascadeType.REMOVE)
