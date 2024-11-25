14bbbb7eb364eafcd110607a749ecb2b26ea03a0
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-25 13:28:42 +0900

    feat: 예시 컨트롤러 수정

--- src/main/java/com/doittogether/platform/presentation/__ResponseApiExampleController.java
+++ src/main/java/com/doittogether/platform/presentation/__ResponseApiExampleController.java
@@ -2,16 +2,17 @@ package com.doittogether.platform.presentation;
 
 import com.doittogether.platform.application.global.ApiResponse;
 import com.doittogether.platform.application.global.code.ExceptionCode;
-import com.doittogether.platform.application.global.exception.housework.HouseworkException;
 import com.doittogether.platform.application.global.code.SuccessCode;
+import com.doittogether.platform.application.global.exception.housework.HouseworkException;
+import com.doittogether.platform.domain.entity.User;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 다양한 형태의 Response API 활용 방법 예제 제공
- * ※ 다음과 같이 프론트에게 매핑하여 전달합니다!
+ * 다양한 형태의 Response API 활용 방법 예제 제공 ※ 다음과 같이 프론트에게 매핑하여 전달합니다!
  *
  * @author ycjung
  */
@@ -20,14 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class __ResponseApiExampleController {
 
     @GetMapping("/success-custom") // 커스텀 성공 처리
-    public ResponseEntity<ApiResponse<String>> shouldReturnSuccessCustom() {
+    public ResponseEntity<ApiResponse<String>> shouldReturnSuccessCustom(@AuthenticationPrincipal User user) {
+        System.out.println(user.getEmail());
         String result = "성공적인 커스텀 생성";
         return ApiResponse.onSuccess(SuccessCode._CREATED, result);
     }
 
     @GetMapping("/error-custom") // 커스텀 실패 처리
     public ResponseEntity<ApiResponse<Void>> shouldReturnErrorCustom() {
-        if(true) {
+        if (true) {
             throw new HouseworkException(ExceptionCode._INTERNAL_SERVER_ERROR);
         }
 
@@ -37,7 +39,7 @@ public class __ResponseApiExampleController {
     @GetMapping("/null-pointer") // NullPointerException 발생
     public ResponseEntity<ApiResponse<Void>> shouldReturnNullPointerException() {
         String str = null;
-        if(str.equals("test")) { // str이 null이므로 NullPointerException 발생
+        if (str.equals("test")) { // str이 null이므로 NullPointerException 발생
             throw new HouseworkException(ExceptionCode._INTERNAL_SERVER_ERROR);
         }
 
