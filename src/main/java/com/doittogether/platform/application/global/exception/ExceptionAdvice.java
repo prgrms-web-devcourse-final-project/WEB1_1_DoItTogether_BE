1d50dee302c130b74b5093c4fb263b27955f1968
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-25 21:38:38 +0900

    feat: 익셉션 핸들러 클래스명 수정

--- src/main/java/com/doittogether/platform/application/global/exception/ExceptionAdvice.java
+++ src/main/java/com/doittogether/platform/application/global/exception/ExceptionAdvice.java
@@ -1,6 +1,7 @@
 package com.doittogether.platform.application.global.exception;
 
-import com.doittogether.platform.application.global.ApiResponse;
+import com.doittogether.platform.application.global.response.BaseResponse;
+import com.doittogether.platform.application.global.response.ExceptionResponse;
 import com.doittogether.platform.application.global.code.ExceptionCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,15 +10,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleUnexpectedException(final Exception exception) {
+    public ResponseEntity<BaseResponse<Void>> handleUnexpectedException(final Exception exception) {
         final ExceptionCode undefinedExceptionCode = ExceptionCode._INTERNAL_SERVER_ERROR;
-        final ApiResponse<Void> body = ApiResponse.onFailure(undefinedExceptionCode);
+        final BaseResponse<Void> body = ExceptionResponse.onFailure(undefinedExceptionCode);
         return new ResponseEntity<>(body, undefinedExceptionCode.getHttpStatus());
     }
 
     @ExceptionHandler(GlobalException.class)
-    public ResponseEntity<ApiResponse<Void>> handleGeneralException(final GlobalException exception) {
-        ApiResponse<Void> body = ApiResponse.onFailure(exception.getExceptionCode());
+    public ResponseEntity<BaseResponse<Void>> handleGeneralException(final GlobalException exception) {
+        BaseResponse<Void> body = ExceptionResponse.onFailure(exception.getExceptionCode());
         return new ResponseEntity<>(body, exception.getExceptionCode().getHttpStatus());
     }
 }
