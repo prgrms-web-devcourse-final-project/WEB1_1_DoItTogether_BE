3ba59f7c6aa6aca4f1e263e01feed09c5698d49e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-25 21:39:49 +0900

    test: 설계 변경으로 인한 test 코드 수정

--- src/test/java/com/doittogether/platform/application/global/ApiResponseTest.java
+++ src/test/java/com/doittogether/platform/application/global/ApiResponseTest.java
@@ -3,78 +3,51 @@ package com.doittogether.platform.application.global;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.doittogether.platform.application.global.code.ExceptionCode;
+import com.doittogether.platform.application.global.code.GlobalCode;
 import com.doittogether.platform.application.global.code.SuccessCode;
-import org.junit.jupiter.api.Test;
+import com.doittogether.platform.application.global.response.BaseResponse;
+import com.doittogether.platform.application.global.response.ExceptionResponse;
+import com.doittogether.platform.application.global.response.SuccessResponse;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.mockito.Mock;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
 class ApiResponseTest {
     @Mock
-    private ApiResponse apiResponse;
+    private BaseResponse apiResponse;
 
     @ParameterizedTest
-    @CsvSource(value = {
-            "_OK:true:OK:COMMON_200:성공입니다.",
-            "_CREATED:true:CREATED:COMMON_201:리소스가 성공적으로 생성되었습니다.",
-            "_NO_CONTENT:true:NO_CONTENT:COMMON_204:콘텐츠가 없습니다."
-    }, delimiter = ':')
-    void 반환하는_데이터가_있는_경우_성공시에는_응답객체를_반환한다(final SuccessCode successCode, final boolean isSuccess,
-                                          final HttpStatus status, final String code, final String message) {
-        final String result = "TestResult";
-        ResponseEntity<ApiResponse<String>> response = ApiResponse.onSuccess(successCode, result);
-        ApiResponse<String> apiResponse = response.getBody();
+    @EnumSource(SuccessCode.class)
+    void 성공_응답_테스트(final SuccessCode successCode) {
 
-        assertApiResponse(apiResponse, isSuccess, status, code, message, result);
-    }
+        BaseResponse<Void> response = SuccessResponse.onSuccess(successCode);
 
-
-    @Test
-    void 반환데이터가_없는_경우_성공시에는_SuccessCode를_기반으로_응답객체를_반환한다() {
-        ResponseEntity<ApiResponse<Void>> response = ApiResponse.onSuccess();
-        ApiResponse<Void> apiResponse = response.getBody();
-
-        assertApiResponse(
-                apiResponse,
-                true,
-                SuccessCode._OK.getHttpStatus(),
-                SuccessCode._OK.getCode(),
-                SuccessCode._OK.getMessage(),
-                null
-        );
+        assertApiResponse(response, successCode, null);
     }
 
     @ParameterizedTest
     @EnumSource(ExceptionCode.class)
     void 실패시에는_적절한_데이터를_반환한다(final ExceptionCode exceptionCode) {
-        ApiResponse<Void> apiResponse = ApiResponse.onFailure(exceptionCode);
 
-        assertApiResponse(
-                apiResponse,
-                false,
-                exceptionCode.getHttpStatus(),
-                exceptionCode.getCode(),
-                exceptionCode.getMessage(),
-                null
-        );
+        BaseResponse<Void> apiResponse = ExceptionResponse.onFailure(exceptionCode);
+
+        assertApiResponse(apiResponse, exceptionCode, null);
     }
 
     private <T> void assertApiResponse(
-            ApiResponse<T> apiResponse,
-            boolean isSuccess,
-            HttpStatus status,
-            String code,
-            String message,
+            BaseResponse<T> apiResponse,
+            GlobalCode code,
             T result
     ) {
+        boolean isSucceed = false;
+        if (apiResponse instanceof SuccessResponse<T>) {
+            isSucceed = true;
+        }
         assertThat(apiResponse).isNotNull();
-        assertThat(apiResponse.isSuccess()).isEqualTo(isSuccess);
-        assertThat(apiResponse.httpStatus()).isEqualTo(status);
-        assertThat(apiResponse.code()).isEqualTo(code);
-        assertThat(apiResponse.message()).isEqualTo(message);
+        assertThat(apiResponse.isSuccess()).isEqualTo(isSucceed);
+        assertThat(apiResponse.httpStatus()).isEqualTo(code.getHttpStatus());
+        assertThat(apiResponse.code()).isEqualTo(code.getCode());
+        assertThat(apiResponse.message()).isEqualTo(code.getMessage());
         assertThat(apiResponse.result()).isEqualTo(result);
     }
 }
