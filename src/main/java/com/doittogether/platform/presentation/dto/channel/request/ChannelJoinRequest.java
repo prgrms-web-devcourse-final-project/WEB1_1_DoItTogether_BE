4fb1a85bd49fb9d269f8b677dfe9862c34826fb0
Yechan Jung <skfk286@hanmail.net>
2024-11-25 18:33:39 +0900

    refactor : 초대 코드 -> 초대 링크 로 네이밍 수정

--- src/main/java/com/doittogether/platform/presentation/dto/channel/request/ChannelJoinRequest.java
+++ src/main/java/com/doittogether/platform/presentation/dto/channel/request/ChannelJoinRequest.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 
 @Schema(description = "채널 입장 요청")
 public record ChannelJoinRequest(
-        @NotBlank(message = "초대 코드는 필수 입력 값입니다.")
-        @Schema(description = "초대 코드")
-        String inviteCode
+        @NotBlank(message = "초대 링크는 필수 입력 값입니다.")
+        @Schema(description = "초대 링크")
+        String inviteLink
 ) {
 }
