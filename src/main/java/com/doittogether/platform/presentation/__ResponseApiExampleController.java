6b9cd9cf861f3cbbe54ad5e1e55964a9649d73ce
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-25 21:41:18 +0900

    docs: 설계 변경으로 인한 예시 컨트롤러 수정

--- src/main/java/com/doittogether/platform/presentation/__ResponseApiExampleController.java
+++ src/main/java/com/doittogether/platform/presentation/__ResponseApiExampleController.java
@@ -1,17 +1,17 @@
 package com.doittogether.platform.presentation;
 
-import com.doittogether.platform.application.global.ApiResponse;
 import com.doittogether.platform.application.global.code.ExceptionCode;
-import com.doittogether.platform.application.global.exception.housework.HouseworkException;
 import com.doittogether.platform.application.global.code.SuccessCode;
+import com.doittogether.platform.application.global.exception.housework.HouseworkException;
+import com.doittogether.platform.application.global.response.BaseResponse;
+import com.doittogether.platform.application.global.response.SuccessResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 다양한 형태의 Response API 활용 방법 예제 제공
- * ※ 다음과 같이 프론트에게 매핑하여 전달합니다!
+ * 다양한 형태의 Response API 활용 방법 예제 제공 ※ 다음과 같이 프론트에게 매핑하여 전달합니다!
  *
  * @author ycjung
  */
@@ -20,38 +20,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class __ResponseApiExampleController {
 
     @GetMapping("/success-custom") // 커스텀 성공 처리
-    public ResponseEntity<ApiResponse<String>> shouldReturnSuccessCustom() {
+    public ResponseEntity<BaseResponse<String>> shouldReturnSuccessCustom() {
         String result = "성공적인 커스텀 생성";
-        return ApiResponse.onSuccess(SuccessCode._CREATED, result);
+        return ResponseEntity.ok()
+                .body(SuccessResponse.onSuccess(SuccessCode._CREATED, result));
     }
 
     @GetMapping("/error-custom") // 커스텀 실패 처리
-    public ResponseEntity<ApiResponse<Void>> shouldReturnErrorCustom() {
-        if(true) {
+    public ResponseEntity<BaseResponse<Void>> shouldReturnErrorCustom() {
+        if (true) {
             throw new HouseworkException(ExceptionCode._INTERNAL_SERVER_ERROR);
         }
-
-        return ApiResponse.onSuccess();
+        return ResponseEntity.ok()
+                .body(SuccessResponse.onSuccess(SuccessCode._OK));
     }
 
     @GetMapping("/null-pointer") // NullPointerException 발생
-    public ResponseEntity<ApiResponse<Void>> shouldReturnNullPointerException() {
+    public ResponseEntity<BaseResponse<Void>> shouldReturnNullPointerException() {
         String str = null;
-        if(str.equals("test")) { // str이 null이므로 NullPointerException 발생
+        if (str.equals("test")) { // str이 null이므로 NullPointerException 발생
             throw new HouseworkException(ExceptionCode._INTERNAL_SERVER_ERROR);
         }
 
-        return ApiResponse.onSuccess();
+        return ResponseEntity.ok()
+                .body(SuccessResponse.onSuccess(SuccessCode._OK));
     }
 
     @GetMapping("/throw")
-    public ResponseEntity<ApiResponse<Void>> shouldReturnThrowException() {
+    public ResponseEntity<BaseResponse<Void>> shouldReturnThrowException() {
         try {
             int i = 1 / 0;
         } catch (ArithmeticException e) {
             throw new HouseworkException(ExceptionCode._INTERNAL_SERVER_ERROR);
         }
 
-        return ApiResponse.onSuccess();
+        return ResponseEntity.ok()
+                .body(SuccessResponse.onSuccess(SuccessCode._OK));
     }
 }
