e134bbc1d677a0e0b21f60019d119e5bd01c8cd8
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-26 09:46:35 +0900

    refactor: 서비스 인터페이스로 변경

---
+++ src/main/java/com/doittogether/platform/business/HouseworkService.java
@@ -0,0 +1,17 @@
+package com.doittogether.platform.business;
+
+import com.doittogether.platform.domain.entity.Housework;
+import com.doittogether.platform.presentation.dto.housework.HouseworkRequest;
+import java.util.List;
+
+public interface HouseworkService {
+    public List<Housework> getAllHouseworks();
+
+    public List<Housework> getHouseworksByCategory(final String category);
+
+    public Housework addHousework(final Housework housework);
+
+    public void updateHousework(final Long id, final HouseworkRequest request);
+
+    public void deleteHousework(final Long id);
+}
