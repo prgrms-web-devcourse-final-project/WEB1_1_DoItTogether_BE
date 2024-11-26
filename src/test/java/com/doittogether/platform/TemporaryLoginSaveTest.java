4e4cdbb4c2c8a968e77b714c2273ca2795891dfb
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-27 00:26:43 +0900

    test: 조회 테스트 추가

--- src/test/java/com/doittogether/platform/TemporaryLoginSaveTest.java
+++ src/test/java/com/doittogether/platform/TemporaryLoginSaveTest.java
@@ -5,8 +5,12 @@ import com.doittogether.platform.domain.entity.User;
 import com.doittogether.platform.infrastructure.persistence.ProfileImageRepository;
 import com.doittogether.platform.infrastructure.persistence.UserRepository;
 import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
@@ -14,6 +18,7 @@ import org.springframework.test.annotation.Rollback;
 @SpringBootTest
 @Transactional
 @Rollback(false)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TemporaryLoginSaveTest {
 
     @Autowired
@@ -24,21 +29,30 @@ public class TemporaryLoginSaveTest {
 
     private ProfileImage profileImage;
 
+    private User user;
+
     @BeforeEach
     public void 기본이미지_저장() {
-         profileImage = ProfileImage.builder()
+        profileImage = ProfileImage.builder()
                 .url("https://www.google.com/imgres?q=기본이미지&imgurl=https%3A%2F%2Fi.pinimg.com%2F474x%2F2f%2F55%2F97%2F2f559707c3b04a1964b37856f00ad608.jpg&imgrefurl=https%3A%2F%2Fkr.pinterest.com%2Fneane1204%2F%25EC%25B9%25B4%25ED%2586%25A1-%25EA%25B8%25B0%25EB%25B3%25B8%25ED%2594%2584%25EC%2582%25AC%2F&docid=-_QnKMphr9-4YM&tbnid=noP947Cj2lUIlM&vet=12ahUKEwjI_72rsvaJAxVQkK8BHYvhBGMQM3oECBoQAA..i&w=398&h=398&hcb=2&ved=2ahUKEwjI_72rsvaJAxVQkK8BHYvhBGMQM3oECBoQAA")
                 .build();
         profileImageRepository.save(profileImage);
     }
 
     @Test
+    @Order(value = 1)
     public void 홍길동_계정_저장() {
-        userRepository.save(
-                User.builder()
-                        .email("doto@gmail.com")
-                        .profileImage(profileImage)
-                        .build()
-        );
+        user = User.builder()
+                .email("doto@gmail.com")
+                .profileImage(profileImage)
+                .build();
+        userRepository.save(user);
+    }
+
+    @Test
+    @Order(value = 2)
+    void 홍길동_계정_조회() {
+        User actual = userRepository.findByEmail("doto@gmail.com").orElseThrow(() -> new RuntimeException("비상"));
+        Assertions.assertThat(actual.getEmail()).isEqualTo("doto@gmail.com");
     }
 }
