fe173eaa4125aecfe4463c5dd665b9411a97f518
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-27 00:27:02 +0900

    fix: response객체 바뀐거 반영

--- src/test/java/com/doittogether/platform/application/global/response/SuccessResponseTest.java
+++ src/test/java/com/doittogether/platform/application/global/response/SuccessResponseTest.java
@@ -12,7 +12,7 @@ class SuccessResponseTest {
     @ParameterizedTest
     @EnumSource(SuccessCode.class)
     void 성공_응답_반환값이_없는_경우(SuccessCode successCode) {
-        BaseResponse<Void> response = SuccessResponse.onSuccess(successCode);
+        SuccessResponse<Void> response = SuccessResponse.onSuccess(successCode);
         assertCommonSuccessResponse(response, successCode);
         assertThat(response.result()).isEqualTo(null);
     }
@@ -21,19 +21,19 @@ class SuccessResponseTest {
     @EnumSource(SuccessCode.class)
     void 성공_응답_반환값이_있는_경우(SuccessCode successCode) {
         final String result = "result";
-        BaseResponse<String> response = SuccessResponse.onSuccess(successCode, result);
+        SuccessResponse<String> response = SuccessResponse.onSuccess(successCode, result);
         assertCommonSuccessResponse(response, successCode);
         assertThat(response.result()).isEqualTo(result);
     }
 
     @Test
     void 성공_응답_반환값도_없고_상태도_결정할_필요가_없는_경우() {
-        BaseResponse<Void> response = SuccessResponse.onSuccess();
+        SuccessResponse<Void> response = SuccessResponse.onSuccess();
         assertCommonSuccessResponse(response, SuccessCode._OK);
         assertThat(response.result()).isEqualTo(null);
     }
 
-    private <T> void assertCommonSuccessResponse(BaseResponse<T> actual, SuccessCode expectedCode) {
+    private <T> void assertCommonSuccessResponse(SuccessResponse<T> actual, SuccessCode expectedCode) {
         assertThat(actual.isSuccess()).isTrue();
         assertThat(actual.httpStatus()).isEqualTo(expectedCode.getHttpStatus());
         assertThat(actual.code()).isEqualTo(expectedCode.getCode());
