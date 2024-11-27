b9bc9489e199b924149fa45576c431d80a400287
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-28 05:50:15 +0900

    refactor: gettr 메서드 명 변경 & 정적 팩토리 메서드 추가

--- src/main/java/com/doittogether/platform/domain/entity/Housework.java
+++ src/main/java/com/doittogether/platform/domain/entity/Housework.java
@@ -1,35 +1,49 @@
 package com.doittogether.platform.domain.entity;
 
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
-
 import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
+import com.doittogether.platform.presentation.dto.housework.HouseworkRequest;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Housework extends BaseEntity {
     @Id
+    @Column(name = "housework_id")
     @GeneratedValue(strategy = IDENTITY)
     private Long houseworkId;
 
+    @Column(name = "start_date_time")
     private LocalDateTime startDateTime;
 
+    @Column(name = "task")
     private String task;
 
+    @Column(name = "category")
+    @Enumerated(value = EnumType.STRING)
     private HouseworkCategory category;
 
+    @Column(name = "status")
+    @Enumerated(value = EnumType.STRING)
     private Status status;
 
+    @Column(name = "is_all_day")
+    private boolean isAllDay;
+
     @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "user_id")
-    private User assignee;
+    @JoinColumn(name = "assignee_id")
+    private Assignee assignee;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "channel_id")
@@ -37,5 +51,44 @@ public class Housework extends BaseEntity {
 
     public boolean isAllocator(User user) {
         return this.assignee == user;
+    public static Housework of(LocalDateTime startDateTime, String task, HouseworkCategory category, Assignee assignee,
+                               Channel channel) {
+        final Housework housework = new Housework();
+        housework.startDateTime = startDateTime;
+        housework.task = task;
+        housework.category = category;
+        housework.status = Status.UN_COMPLETE;
+        housework.assignee = assignee;
+        housework.channel = channel;
+        return housework;
+    }
+    }
+
+    public LocalDateTime retrieveStartDateTime() {
+        return startDateTime;
+    }
+
+    public String retrieveTask() {
+        return task;
+    }
+
+    public HouseworkCategory retrieveCategory() {
+        return category;
+    }
+
+    public Status retrieveStatus() {
+        return status;
+    }
+
+    public boolean isAllDay() {
+        return isAllDay;
+    }
+
+    public Assignee retrieveAssignee() {
+        return assignee;
+    }
+
+    public Channel retrieveChannel() {
+        return channel;
     }
 }
