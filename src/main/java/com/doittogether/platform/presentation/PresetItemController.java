6190575156ba96c4010f4f27edd79b5f1d26ef02
Yechan Jung <skfk286@hanmail.net>
2024-11-27 11:50:41 +0900

    refactor : Controller 리턴 타입 및 PathVariable name 지정

--- src/main/java/com/doittogether/platform/presentation/PresetItemController.java
+++ src/main/java/com/doittogether/platform/presentation/PresetItemController.java
@@ -1,6 +1,7 @@
 package com.doittogether.platform.presentation;
 
-import com.doittogether.platform.application.global.ApiResponse;
+import com.doittogether.platform.application.global.code.SuccessCode;
+import com.doittogether.platform.application.global.response.SuccessResponse;
 import com.doittogether.platform.presentation.dto.preset.request.PresetDeleteRequest;
 import com.doittogether.platform.presentation.dto.preset.request.PresetItemRegisterRequest;
 import com.doittogether.platform.presentation.dto.preset.request.PresetItemUpdateRequest;
@@ -10,49 +11,54 @@ import com.doittogether.platform.presentation.dto.preset.response.PresetItemRegi
 import com.doittogether.platform.presentation.dto.preset.response.PresetItemUpdateResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/presets")
+@RequestMapping("/api/v1/presets/{channelId}")
 @Tag(name = "프리셋 아이템 API", description = "프리셋 아이템 관리 API")
 public class PresetItemController {
 
-    @PostMapping("/{channelId}")
+    @PostMapping
     @Operation(summary = "프리셋 아이템 생성", description = "지정된 채널 ID에 새로운 프리셋 아이템을 생성합니다.")
-    public ResponseEntity<ApiResponse<PresetItemRegisterResponse>> createPreset(
-            @PathVariable Long channelId, @RequestBody PresetItemRegisterRequest request
+    public ResponseEntity<SuccessResponse<PresetItemRegisterResponse>> createPreset(
+            @PathVariable("channelId") Long channelId, @RequestBody PresetItemRegisterRequest request
     ) {
 
-        return ApiResponse.onSuccess(null);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.onSuccess(SuccessCode._OK, null));
     }
 
-    @PutMapping("/{channelId}")
+    @PutMapping
     @Operation(summary = "프리셋 아이템 수정", description = "지정된 채널 ID의 기존 프리셋 아이템을 수정합니다.")
-    public ResponseEntity<ApiResponse<PresetItemUpdateResponse>> updatePreset(
-            @PathVariable Long channelId, @RequestBody PresetItemUpdateRequest request
+    public ResponseEntity<SuccessResponse<PresetItemUpdateResponse>> updatePreset(
+            @PathVariable("channelId") Long channelId, @RequestBody PresetItemUpdateRequest request
     ) {
 
-        return ApiResponse.onSuccess(null);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.onSuccess(SuccessCode._OK, null));
     }
 
-    @DeleteMapping("/{channelId}/{presetItemId}")
+    @DeleteMapping("/{presetItemId}")
     @Operation(summary = "프리셋 아이템 삭제", description = "지정된 채널 ID 및 프리셋 아이템 ID를 기반으로 프리셋 아이템을 삭제합니다.")
-    public ResponseEntity<ApiResponse<PresetItemDeleteResponse>> deletePreset(
-            @PathVariable Long channelId, @PathVariable Long presetItemId
+    public ResponseEntity<SuccessResponse<PresetItemDeleteResponse>> deletePreset(
+            @PathVariable("channelId") Long channelId, @PathVariable Long presetItemId
     ) {
 
-        return ApiResponse.onSuccess(null);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.onSuccess(SuccessCode._OK, null));
     }
 
-    @DeleteMapping("/{channelId}/{presetItemId}/details")
+    @DeleteMapping("/{presetItemId}/details")
     @Operation(summary = "프리셋 세부 항목 삭제",
             description = "지정된 채널 ID 및 프리셋 아이템 ID의 세부 항목을 삭제합니다. 삭제할 세부 항목은 요청 본문에서 제공합니다.")
-    public ResponseEntity<ApiResponse<PresetDeleteResponse>> deletePresetDetail(
-            @PathVariable Long channelId, @PathVariable Long presetItemId,
+    public ResponseEntity<SuccessResponse<PresetDeleteResponse>> deletePresetDetail(
+            @PathVariable("channelId") Long channelId, @PathVariable Long presetItemId,
             @RequestBody PresetDeleteRequest request
     ) {
-        return ApiResponse.onSuccess(null);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(SuccessResponse.onSuccess(SuccessCode._OK, null));
     }
 
 }
