442b71e1f3e02dcf9c4f43781085a963edb57a45
Yechan Jung <skfk286@hanmail.net>
2024-11-27 10:25:06 +0900

    refactor : 유저 join response 포멧 수정

--- src/main/java/com/doittogether/platform/presentation/dto/channel/response/ChannelJoinResponse.java
+++ src/main/java/com/doittogether/platform/presentation/dto/channel/response/ChannelJoinResponse.java
@@ -1,5 +1,7 @@
 package com.doittogether.platform.presentation.dto.channel.response;
 
+import com.doittogether.platform.domain.entity.Channel;
+import com.doittogether.platform.domain.entity.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
@@ -15,10 +17,10 @@ public record ChannelJoinResponse(
         @Schema(description = "사용자가 성공적으로 입장했는지 여부", example = "true")
         boolean joinedSuccessfully
 ) {
-    public static ChannelJoinResponse of(Long channelId, String name, boolean joinedSuccessfully) {
+    public static ChannelJoinResponse of(Channel channel, boolean joinedSuccessfully) {
         return ChannelJoinResponse.builder()
-                .channelId(channelId)
-                .name(name)
+                .channelId(channel.getChannelId())
+                .name(channel.getName())
                 .joinedSuccessfully(joinedSuccessfully)
                 .build();
     }
