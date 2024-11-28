e2ef66bfce737cf153273851eaa702735192a014
Yechan Jung <skfk286@hanmail.net>
2024-11-29 00:38:23 +0900

    refactor : 테스트 코드에서 생성용 builder 추가

--- src/main/java/com/doittogether/platform/presentation/dto/preset/request/PresetCategoryRegisterRequest.java
+++ src/main/java/com/doittogether/platform/presentation/dto/preset/request/PresetCategoryRegisterRequest.java
@@ -3,8 +3,10 @@ package com.doittogether.platform.presentation.dto.preset.request;
 import com.doittogether.platform.domain.entity.Channel;
 import com.doittogether.platform.domain.entity.PresetCategory;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 @Schema(description = "프리셋 카테고리 등록 요청")
+@Builder
 public record PresetCategoryRegisterRequest(
         @Schema(description = "카테고리 이름", example = "거실")
         String category
--- src/main/java/com/doittogether/platform/presentation/dto/preset/request/PresetItemRegisterRequest.java
+++ src/main/java/com/doittogether/platform/presentation/dto/preset/request/PresetItemRegisterRequest.java
@@ -3,14 +3,16 @@ package com.doittogether.platform.presentation.dto.preset.request;
 import com.doittogether.platform.domain.entity.PresetCategory;
 import com.doittogether.platform.domain.entity.PresetItem;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 
 @Schema(description = "프리셋 아이템 등록 요청")
+@Builder
 public record PresetItemRegisterRequest(
         @Schema(description = "프리셋 아이템 이름", example = "[\"바닥 청소\", \"유리창 닦기\"]")
-        String item
+        String name
 ) {
     public static PresetItem toEntity(PresetItemRegisterRequest request,
                                       PresetCategory category) {
-        return PresetItem.of(request.item, category);
+        return PresetItem.of(request.name, category);
     }
 }
