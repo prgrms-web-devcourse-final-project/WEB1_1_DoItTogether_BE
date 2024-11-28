d112975f3d4b985c91505f5436e671523165cb6e
Yechan Jung <skfk286@hanmail.net>
2024-11-29 00:00:58 +0900

    refactor : 프리셋 카테고리 jpql 조회 쿼리 추가

--- src/main/java/com/doittogether/platform/infrastructure/persistence/PresetCategoryRepository.java
+++ src/main/java/com/doittogether/platform/infrastructure/persistence/PresetCategoryRepository.java
@@ -1,7 +1,14 @@
 package com.doittogether.platform.infrastructure.persistence;
 
 import com.doittogether.platform.domain.entity.PresetCategory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PresetCategoryRepository extends JpaRepository<PresetCategory, Long> {
+    @Query("SELECT c FROM PresetCategory c WHERE c.channel.channelId = :channelId")
+    List<PresetCategory> findAllByChannelId(@Param("channelId") Long channelId, Pageable pageable);
 }
