bdede49016644ac489da92324ccc11152ab6bbc0
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-28 15:51:25 +0900

    fix: 서비스 메소드 명 변경 & 파라미터 변경

--- src/main/java/com/doittogether/platform/business/housework/HouseworkService.java
+++ src/main/java/com/doittogether/platform/business/housework/HouseworkService.java
@@ -1,17 +1,22 @@
 package com.doittogether.platform.business.housework;
 
-import com.doittogether.platform.domain.entity.Housework;
+import com.doittogether.platform.domain.entity.User;
 import com.doittogether.platform.presentation.dto.housework.HouseworkRequest;
-import java.util.List;
+import com.doittogether.platform.presentation.dto.housework.HouseworkSliceResponse;
+import org.springframework.data.domain.PageRequest;
 
 public interface HouseworkService {
-    public List<Housework> getAllHouseworks();
+    public HouseworkSliceResponse findAllByChannelId(final User loginUser, final Long lastHouseworkId, final Long channelId,
+                                                     final PageRequest pageRequest);
 
-    public List<Housework> getHouseworksByCategory(final String category);
+    public HouseworkSliceResponse findAllByChannelIdAndAssigneeId(final Long lastHouseworkId, final Long channelId,
+                                                                  final Long userId, final
+                                                                  PageRequest pageRequest);
 
-    public Housework addHousework(final Housework housework);
+    public void addHousework(final Long channelId, final HouseworkRequest request);
 
-    public void updateHousework(final Long id, final HouseworkRequest request);
+    public void updateHousework(final User loginUser, final Long houseworkId, final Long channelId,
+                                final HouseworkRequest request);
 
-    public void deleteHousework(final Long id);
+    public void deleteHousework(final User loginUser, final Long houseworkId, final Long channelId);
 }
