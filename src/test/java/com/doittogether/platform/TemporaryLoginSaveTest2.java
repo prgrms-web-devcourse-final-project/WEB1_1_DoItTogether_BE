ebfd434177cab290de90b3f208b8531792bd4e1a
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-28 15:52:45 +0900

    refactor: 메서드 명 변경으로 인한 테스트코드 수정

--- src/test/java/com/doittogether/platform/TemporaryLoginSaveTest2.java
+++ src/test/java/com/doittogether/platform/TemporaryLoginSaveTest2.java
@@ -34,19 +34,14 @@ public class TemporaryLoginSaveTest2 {
 
     @BeforeEach
     public void 기본이미지_저장() {
-         profileImage = ProfileImage.builder()
-                .url("https://www.google.com/imgres?q=기본이미지&imgurl=https%3A%2F%2Fi.pinimg.com%2F474x%2F2f%2F55%2F97%2F2f559707c3b04a1964b37856f00ad608.jpg&imgrefurl=https%3A%2F%2Fkr.pinterest.com%2Fneane1204%2F%25EC%25B9%25B4%25ED%2586%25A1-%25EA%25B8%25B0%25EB%25B3%25B8%25ED%2594%2584%25EC%2582%25AC%2F&docid=-_QnKMphr9-4YM&tbnid=noP947Cj2lUIlM&vet=12ahUKEwjI_72rsvaJAxVQkK8BHYvhBGMQM3oECBoQAA..i&w=398&h=398&hcb=2&ved=2ahUKEwjI_72rsvaJAxVQkK8BHYvhBGMQM3oECBoQAA")
-                .build();
+         profileImage = ProfileImage.from("\"https://www.google.com/imgres?q=기본이미지&imgurl=https%3A%2F%2Fi.pinimg.com%2F474x%2F2f%2F55%2F97%2F2f559707c3b04a1964b37856f00ad608.jpg&imgrefurl=https%3A%2F%2Fkr.pinterest.com%2Fneane1204%2F%25EC%25B9%25B4%25ED%2586%25A1-%25EA%25B8%25B0%25EB%25B3%25B8%25ED%2594%2584%25EC%2582%25AC%2F&docid=-_QnKMphr9-4YM&tbnid=noP947Cj2lUIlM&vet=12ahUKEwjI_72rsvaJAxVQkK8BHYvhBGMQM3oECBoQAA..i&w=398&h=398&hcb=2&ved=2ahUKEwjI_72rsvaJAxVQkK8BHYvhBGMQM3oECBoQAA");
         profileImageRepository.save(profileImage);
     }
 
     @Test
     public void 홍길동_계정_저장() {
         userRepository.save(
-                User.builder()
-                        .email("doto@gmail.com")
-                        .profileImage(profileImage)
-                        .build()
+                User.of("길똥이","doto@gmail.com", profileImage)
         );
     }
 }
