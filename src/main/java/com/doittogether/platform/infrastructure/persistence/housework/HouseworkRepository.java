3d8ae60b8e0df816cbe8e831d18d0210c00bd4a8
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-29 23:06:07 +0900

    feat: no-offset 삭제

--- src/main/java/com/doittogether/platform/infrastructure/persistence/housework/HouseworkRepository.java
+++ src/main/java/com/doittogether/platform/infrastructure/persistence/housework/HouseworkRepository.java
@@ -1,6 +1,7 @@
 package com.doittogether.platform.infrastructure.persistence.housework;
 
 import com.doittogether.platform.domain.entity.Housework;
+import java.time.LocalDateTime;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,8 +10,8 @@ import org.springframework.data.repository.query.Param;
 
 public interface HouseworkRepository extends JpaRepository<Housework, Long> {
     @Query(value = "select h from Housework h where "
-            + ":lastHouseworkId = 0 OR h.houseworkId < :lastHouseworkId "
-            + "and h.channel.channelId = :channelId "
+            + "h.channel.channelId = :channelId "
+            + "and h.startDateTime BETWEEN :startOfDay AND :endOfDay "
             + "order by "
             + "case "
             + "when h.status = 'UN_COMPLETE' then 0 "
@@ -22,27 +23,26 @@ public interface HouseworkRepository extends JpaRepository<Housework, Long> {
             + "else 1 "
             + "end asc,"
             + "h.createdAt desc")
-    Slice<Housework> findAllByChannelId(@Param("lastHouseworkId") final Long lastHouseworkId,
-                                        @Param("channelId") final Long channelId, @Param("userId") final Long userId,
-                                        final
-                                        Pageable pageable);
+    Slice<Housework> findAllByChannelIdAndTargetDate(@Param("channelId") final Long channelId,
+                                                     @Param("userId") final Long userId,
+                                                     final
+                                                     Pageable pageable,
+                                                     @Param("startOfDay") final LocalDateTime startOfDay,
+                                                     @Param("endOfDay") final LocalDateTime endOfDay);
 
     @Query(value = "select h from Housework h where "
-            + ":lastHouseworkId = 0 OR h.houseworkId < :lastHouseworkId "
-            + "and h.channel.channelId = :channelId "
-            + "and h.assignee.assigneeId = :assigneeId "
+            + "h.assignee.assigneeId = :assigneeId "
             + "order by "
             + "case "
             + "when h.status = 'UN_COMPLETE' then 0 "
             + "when h.status = 'COMPLETE' then 1 "
             + "else 3 "
             + "end asc, "
-            + "case "
-            + "when h.assignee.assigneeId = :assigneeId then 0 "
-            + "else 1 "
-            + "end asc,"
             + "h.createdAt desc")
-    Slice<Housework> findAllByChannelIdAndAssigneeId(final Long channelId,
-                                                     final Long assigneeId, final
-                                                     Pageable pageable);
+    Slice<Housework> findAllByChannelIdAndTargetDateAndAssigneeId(@Param("channelId") final Long channelId,
+                                                                  @Param("assigneeId") final Long assigneeId,
+                                                                  final
+                                                                  Pageable pageable,
+                                                                  @Param("startOfDay") final LocalDateTime startOfDay,
+                                                                  @Param("endOfDay") final LocalDateTime endOfDay);
 }
