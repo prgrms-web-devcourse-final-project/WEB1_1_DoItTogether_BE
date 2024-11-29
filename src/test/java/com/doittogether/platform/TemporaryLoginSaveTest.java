a9e28ccd33a89160d93af433c29e9ac62a1773f2
eundeang <111877048+eundeang@users.noreply.github.com>
2024-11-30 00:40:47 +0900

    test: 저장 두번되는 문제 해결

--- src/test/java/com/doittogether/platform/TemporaryLoginSaveTest.java
+++ src/test/java/com/doittogether/platform/TemporaryLoginSaveTest.java
@@ -44,7 +44,6 @@ public class TemporaryLoginSaveTest {
         userRepository.save(
                 User.of("길똥이", "doto@gmail.com", profileImage)
         );
-        userRepository.save(user);
     }
 
     @Test
