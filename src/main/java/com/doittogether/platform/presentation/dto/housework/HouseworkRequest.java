b12d75567d431a40a062bb6c277ebbc47de4cd87
eundeang <111877048+eundeang@users.noreply.github.com>
2024-12-01 02:10:57 +0900

    feat: json 예시 변경, category String 타입으로 변경

--- src/main/java/com/doittogether/platform/presentation/dto/housework/HouseworkRequest.java
+++ src/main/java/com/doittogether/platform/presentation/dto/housework/HouseworkRequest.java
@@ -1,8 +1,6 @@
 package com.doittogether.platform.presentation.dto.housework;
 
-import com.doittogether.platform.domain.entity.HouseworkCategory;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
@@ -13,15 +11,15 @@ import java.time.LocalTime;
 @Schema(description = "집안일 수정 요청 DTO")
 public record HouseworkRequest(
         @NotNull(message = "카테고리 이름을 입력해주세요.")
-        @Schema(description = "카테고리 이름", example = "LIVING_ROOM")
-        HouseworkCategory category,
+        @Schema(description = "카테고리 이름", example = "거실")
+        String category,
 
         @NotBlank(message = "작업 이름을 입력해주세요.")
         @Schema(description = "작업 이름", example = "먼지 닦기")
         String task,
 
         @NotNull(message = "시작 날짜와 시간을 입력해주세요. 패턴은 yyyy-MM-dd과 같습니다.")
-        @Schema(description = "시작 날짜와 시간", example = "2021-11-30", type = "string")
+        @Schema(description = "시작 날짜와 시간", example = "2024-11-23", type = "string")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         LocalDate startDate,
 
@@ -34,7 +32,7 @@ public record HouseworkRequest(
         @Schema(description = "작업 담당자 ID", example = "1")
         Long userId
 ) {
-    public static HouseworkRequest of(HouseworkCategory category, String task, LocalDate startDate, LocalTime startTime,
+    public static HouseworkRequest of(String category, String task, LocalDate startDate, LocalTime startTime,
                                       Boolean isAllDay,
                                       Long userId) {
         return new HouseworkRequest(category, task, startDate, startTime, userId);
