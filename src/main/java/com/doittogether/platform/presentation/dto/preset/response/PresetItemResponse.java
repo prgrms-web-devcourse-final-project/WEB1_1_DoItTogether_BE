28ce4711a597b4c31a23069a7c85b3e63f3bfd9e
Yechan Jung <skfk286@hanmail.net>
2024-12-01 17:05:49 +0900

    chore : 프리셋 리스폰스 필드명 수정

--- src/main/java/com/doittogether/platform/presentation/dto/preset/response/PresetItemResponse.java
+++ src/main/java/com/doittogether/platform/presentation/dto/preset/response/PresetItemResponse.java
@@ -3,13 +3,13 @@ package com.doittogether.platform.presentation.dto.preset.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
-@Schema(description = "프리셋 세부 항목 응답")
+@Schema(description = "프리셋 아이템 항목 응답")
 @Builder
 public record PresetItemResponse(
         @Schema(description = "프리셋 아이템 아이디")
         Long presetItemId,
 
-        @Schema(description = "프리셋 이름", example = "바닥 청소")
+        @Schema(description = "프리셋 아이템 이름", example = "바닥 청소")
         String name
 ) {
     public static PresetItemResponse of(Long preseItemtId, String name) {
