15bf58e9207bb5f17aae08acfddeda608d85599b
seonghoon <gortmtjdgns0943@gmail.com>
2024-12-02 15:19:25 +0900

    refactor: 로그아웃 메서드 제거 (OAuth2에서 관리하기 때문에 필요 x)

--- src/main/java/com/doittogether/platform/presentation/UserContoller.java
+++ src/main/java/com/doittogether/platform/presentation/UserContoller.java
@@ -7,27 +7,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping()
+@RequestMapping("/api/v1/user/")
 public class UserContoller {
 
-    @RequestMapping("/api/v1/user/update")
+    @RequestMapping("update")
     @Operation(summary = "닉네임 변경", description = "사용자의 닉네임을 변경합니다.")
     public ResponseEntity<SuccessResponse<Void>> updateUserName(@AuthenticationPrincipal CustomOAuth2User user) {
-        System.out.println("닉네임 변경 : " + user.getName());
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(SuccessResponse.onSuccess());
-    }
-
-    @PostMapping("/api/auth/logout")
-    @Operation(summary = "로그아웃", description = "로그아웃을 진행합니다.")
-    public ResponseEntity<SuccessResponse<Void>> logout() {
-
+        // To-do
         return ResponseEntity.status(HttpStatus.OK)
                 .body(SuccessResponse.onSuccess());
     }
