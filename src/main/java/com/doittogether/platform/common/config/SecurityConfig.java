c4dd241c86062aa442d6fb332e8572625e7a674e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-12-03 00:09:29 +0900

    feat: 스프링 시큐리티 활성화 @EnableWebSecurity 적용

--- src/main/java/com/doittogether/platform/common/config/SecurityConfig.java
+++ src/main/java/com/doittogether/platform/common/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.doittogether.platform.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Value("${front.url}")
