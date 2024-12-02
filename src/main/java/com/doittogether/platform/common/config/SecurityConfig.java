fe2e72b05bf1c4816f2dca62888d44f87b214cc7
eundeang <111877048+eundeang@users.noreply.github.com>
2024-12-03 05:28:59 +0900

    feat: security OAuthLogin부분 구현

--- src/main/java/com/doittogether/platform/common/config/SecurityConfig.java
+++ src/main/java/com/doittogether/platform/common/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.doittogether.platform.common.config;
 
-import java.util.Collections;
+import com.doittogether.platform.business.user.CustomAuthenticationSuccessHandler;
+import com.doittogether.platform.common.auth.filter.JwtAuthenticationFilter;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -8,7 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,6 +21,10 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final DefaultOAuth2UserService customOAuth2UserService;
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Value("${front.url}")
     private String frontUrl;
@@ -37,8 +45,13 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> {
                     auth.requestMatchers(permitAllPaths).permitAll();
                     auth.anyRequest().authenticated();
-                });
-
+                })
+                .oauth2Login(oauth2 -> {
+                    oauth2.redirectionEndpoint(endpoint -> endpoint.baseUri("/oauth2/callback/*"));
+                    oauth2.userInfoEndpoint(endpoint -> endpoint.userService(customOAuth2UserService));
+                    oauth2.successHandler(customAuthenticationSuccessHandler);
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
