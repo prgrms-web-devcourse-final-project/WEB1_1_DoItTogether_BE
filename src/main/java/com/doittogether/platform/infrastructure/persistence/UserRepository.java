6496cabff0457f896c3e564b58099c3a4de93a04
seonghoon <gortmtjdgns0943@gmail.com>
2024-12-04 01:25:26 +0900

    feat: 사용자 닉네임 별 프로필 이미지 url 조회 쿼리 메서드 작성

--- src/main/java/com/doittogether/platform/infrastructure/persistence/UserRepository.java
+++ src/main/java/com/doittogether/platform/infrastructure/persistence/UserRepository.java
@@ -1,9 +1,15 @@
 package com.doittogether.platform.infrastructure.persistence;
 
 import com.doittogether.platform.domain.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(final String email);
+
+    @Query("SELECT u.profileImage.url FROM User u WHERE u.nickName = :nickName")
+    Optional<String> findProfileImageUrlByNickName(@Param("nickName") String nickName);
 }
