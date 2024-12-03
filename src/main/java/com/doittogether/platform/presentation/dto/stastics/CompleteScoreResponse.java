86febf173390c1b2ce251edb21658d20b68929ec
seonghoon <gortmtjdgns0943@gmail.com>
2024-12-04 01:28:51 +0900

    feat: 집안일 주간, 원간 관련 통계 응답 객체 정적 팩토리 메서드 구현

--- src/main/java/com/doittogether/platform/presentation/dto/stastics/CompleteScoreResponse.java
+++ src/main/java/com/doittogether/platform/presentation/dto/stastics/CompleteScoreResponse.java
@@ -1,8 +1,17 @@
 package com.doittogether.platform.presentation.dto.stastics;
 
+import lombok.Builder;
+
 import java.util.List;
 
+@Builder
 public record CompleteScoreResponse(
         List<PersonalCompleteScoreResponse> completeScoreSortedResponse
 ) {
+
+    public static CompleteScoreResponse of (List<PersonalCompleteScoreResponse> weeklyStatistics){
+        return CompleteScoreResponse.builder()
+                .completeScoreSortedResponse(weeklyStatistics)
+                .build();
+    }
 }
