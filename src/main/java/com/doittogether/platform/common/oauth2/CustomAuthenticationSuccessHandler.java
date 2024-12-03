854fedd61220482580104e88c8936d8dab1b2833
eundeang <111877048+eundeang@users.noreply.github.com>
2024-12-03 17:55:47 +0900

    feat: 프론트 Query Param으로 전달 요구사항 반영 & 위치 변경

--- src/main/java/com/doittogether/platform/common/oauth2/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/doittogether/platform/common/oauth2/CustomAuthenticationSuccessHandler.java
@@ -1,14 +1,15 @@
-package com.doittogether.platform.business.user;
+package com.doittogether.platform.common.oauth2;
 
 import com.doittogether.platform.common.config.jwt.JwtProvider;
 import com.doittogether.platform.common.config.jwt.UserAuthentication;
 import com.doittogether.platform.domain.entity.User;
 import com.doittogether.platform.infrastructure.persistence.UserRepository;
-import com.doittogether.platform.presentation.dto.user.CustomOAuth2User;
+import com.doittogether.platform.common.oauth2.dto.CustomOAuth2User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -16,6 +17,8 @@ import org.springframework.stereotype.Component;
 @Component
 @RequiredArgsConstructor
 public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    @Value("${front.url}")
+    private String FRONT_SERVER;
 
     private final JwtProvider jwtProvider;
     private final UserRepository userRepository;
@@ -30,8 +33,8 @@ public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         final UserAuthentication userAuthentication = new UserAuthentication(user.retrieveUserId(), null, null);
         final String token = jwtProvider.generateToken(userAuthentication);
 
-        response.setContentType("application/json");
         response.setStatus(HttpServletResponse.SC_OK);
-        response.getWriter().write("{\"authorization\": \"" + token + "\"}");
+        final String redirectUrl = FRONT_SERVER + "?access_token=" + token;
+        response.sendRedirect(redirectUrl);
     }
 }
