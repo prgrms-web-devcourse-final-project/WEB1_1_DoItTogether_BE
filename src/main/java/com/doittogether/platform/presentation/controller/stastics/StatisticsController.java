65af77e580f9b7bd72a4ddc629259f37cfaa083d
seonghoon <gortmtjdgns0943@gmail.com>
2024-12-04 14:52:33 +0900

    refactor: 로그인 인증 방법 변경 및 StatisticsException 추가

--- src/main/java/com/doittogether/platform/presentation/controller/stastics/StatisticsController.java
+++ src/main/java/com/doittogether/platform/presentation/controller/stastics/StatisticsController.java
@@ -1,21 +1,21 @@
 package com.doittogether.platform.presentation.controller.stastics;
 
 import com.doittogether.platform.application.global.response.SuccessResponse;
-import com.doittogether.platform.domain.entity.User;
 import com.doittogether.platform.presentation.dto.stastics.ChannelCountStatisticsResponse;
 import com.doittogether.platform.presentation.dto.stastics.CompleteScoreResponse;
 import com.doittogether.platform.presentation.dto.stastics.MonthlyMVPResponse;
 import com.doittogether.platform.presentation.dto.stastics.MonthlyStatisticsResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.time.LocalDate;
 
 @RequestMapping("/api/v1/channels/{channelId}/statistics")
 public interface StatisticsController {
@@ -23,7 +23,7 @@ public interface StatisticsController {
     @GetMapping("/weekly/{targetDate}/totalCount")
     @Operation(summary = "주간 통계관련 완료 미완료 칭찬 찌르기 개수 조회", description = "주간 통계에서 사용할, 이번주 완료 개수 랭킹을 반환합니다.")
     ResponseEntity<SuccessResponse<ChannelCountStatisticsResponse>> calculateTotalCountByChannelId(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetDate")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -33,7 +33,7 @@ public interface StatisticsController {
     @GetMapping("/weekly/{targetDate}/score")
     @Operation(summary = "주간 통계, 이번주 완료 개수 랭킹", description = "주간통계 중, 이번주 완료 개수 랭킹을 반환합니다.")
     ResponseEntity<SuccessResponse<CompleteScoreResponse>> calculateWeeklyStatistics(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetDate")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
@@ -44,7 +44,7 @@ public interface StatisticsController {
     @GetMapping("/monthly/{targetMonth}/score")
     @Operation(summary = "월간 통계, 캘린더 부분 조회", description = "월간통계 중, 캘린더 부분에 사용될 데이터를 반환합니다.")
     ResponseEntity<SuccessResponse<MonthlyStatisticsResponse>> calculateMonthlyStatistics(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetDate")
             @Parameter(description = "선택 월 (yyyy-MM 형식)", example = "2024-11") String targetDate
@@ -53,7 +53,7 @@ public interface StatisticsController {
     @GetMapping("/monthly/{targetMonth}/score")
     @Operation(summary = "월간 통계,MVP 부분 조회", description = "월간통계 중, MVP 부분에 사용될 데이터를 반환합니다.")
     ResponseEntity<SuccessResponse<MonthlyMVPResponse>> calculateMonthlyMVP(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetMonth")
             @Parameter(description = "선택 월 (yyyy-MM 형식)", example = "2024-11") String targetMonth
--- src/main/java/com/doittogether/platform/presentation/controller/stastics/StatisticsControllerImpl.java
+++ src/main/java/com/doittogether/platform/presentation/controller/stastics/StatisticsControllerImpl.java
@@ -3,6 +3,7 @@ package com.doittogether.platform.presentation.controller.stastics;
 import com.doittogether.platform.application.global.code.SuccessCode;
 import com.doittogether.platform.application.global.response.SuccessResponse;
 import com.doittogether.platform.business.stastics.StatisticsService;
+import com.doittogether.platform.business.user.UserService;
 import com.doittogether.platform.domain.entity.User;
 import com.doittogether.platform.presentation.dto.stastics.ChannelCountStatisticsResponse;
 import com.doittogether.platform.presentation.dto.stastics.CompleteScoreResponse;
@@ -15,10 +16,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.time.LocalDate;
 
 @RestController
@@ -28,18 +29,20 @@ import java.time.LocalDate;
 public class StatisticsControllerImpl implements StatisticsController {
 
     private final StatisticsService statisticsService;
+    private final UserService userService;
 
     @GetMapping("/weekly/{targetDate}/totalCount")
     @Operation(summary = "주간 통계관련 완료 미완료 칭찬 찌르기 개수 조회", description = "주간 통계에서 사용할, 이번주 완료 개수 랭킹을 반환합니다.")
     @Override
     public ResponseEntity<SuccessResponse<ChannelCountStatisticsResponse>> calculateTotalCountByChannelId(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetDate")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             @Parameter(description = "선택 날짜 (yyyy-MM-dd 형식)", example = "2024-11-25") LocalDate targetDate
     ) {
-        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = Long.parseLong(principal.getName());
+        User loginUser = userService.findByIdOrThrow(userId);
         return ResponseEntity.status(HttpStatus.OK).body(
                 SuccessResponse.onSuccess(
                         SuccessCode._OK,
@@ -51,13 +54,14 @@ public class StatisticsControllerImpl implements StatisticsController {
     @Operation(summary = "주간 통계, 이번주 완료 개수 랭킹", description = "주간통계 중, 이번주 완료 개수 랭킹을 반환합니다.")
     @Override
     public ResponseEntity<SuccessResponse<CompleteScoreResponse>> calculateWeeklyStatistics(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetDate")
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
             @Parameter(description = "선택 날짜 (yyyy-MM-dd 형식)", example = "2024-11-25") LocalDate targetDate
     ) {
-        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = Long.parseLong(principal.getName());
+        User loginUser = userService.findByIdOrThrow(userId);
         return ResponseEntity.status(HttpStatus.OK).body(
                 SuccessResponse.onSuccess(
                         SuccessCode._OK,
@@ -69,7 +73,7 @@ public class StatisticsControllerImpl implements StatisticsController {
     @Operation(summary = "월간 통계, 캘린더 부분 조회", description = "월간통계 중, 캘린더 부분에 사용될 데이터를 반환합니다.")
     @Override
     public ResponseEntity<SuccessResponse<MonthlyStatisticsResponse>> calculateMonthlyStatistics(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetMonth")
             @Parameter(description = "선택 월 (yyyy-MM 형식)", example = "2024-11") String targetMonth
@@ -87,12 +91,13 @@ public class StatisticsControllerImpl implements StatisticsController {
     @Operation(summary = "월간 통계,MVP 부분 조회", description = "월간통계 중, MVP 부분에 사용될 데이터를 반환합니다.")
     @Override
     public ResponseEntity<SuccessResponse<MonthlyMVPResponse>> calculateMonthlyMVP(
-            @AuthenticationPrincipal User user,
+            Principal principal,
             @PathVariable("channelId") Long channelId,
             @RequestParam("targetMonth")
             @Parameter(description = "선택 월 (yyyy-MM 형식)", example = "2024-11") String targetMonth
     ) {
-        User loginUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Long userId = Long.parseLong(principal.getName());
+        User loginUser = userService.findByIdOrThrow(userId);
         LocalDate targetDate = LocalDate.parse(targetMonth + "-01");
         return ResponseEntity.status(HttpStatus.OK).body(
                 SuccessResponse.onSuccess(
