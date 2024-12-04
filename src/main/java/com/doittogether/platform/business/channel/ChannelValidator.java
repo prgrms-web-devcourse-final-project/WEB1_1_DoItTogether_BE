58b904c7cd242d3be76a39bc1a5cd19d760a57d7
seonghoon <gortmtjdgns0943@gmail.com>
2024-12-04 22:38:29 +0900

    feat: 로그인 사용자가 수정하려는 채널에 가입했는 지 확인을 위한 Validator 구현

--- src/main/java/com/doittogether/platform/business/channel/ChannelValidator.java
+++ src/main/java/com/doittogether/platform/business/channel/ChannelValidator.java
@@ -2,7 +2,9 @@ package com.doittogether.platform.business.channel;
 
 import com.doittogether.platform.application.global.code.ExceptionCode;
 import com.doittogether.platform.application.global.exception.channel.ChannelValidationException;
-import com.doittogether.platform.application.global.exception.housework.HouseworkException;
+import com.doittogether.platform.domain.entity.Channel;
+import com.doittogether.platform.domain.entity.User;
+import com.doittogether.platform.infrastructure.persistence.UserChannelRepository;
 import com.doittogether.platform.infrastructure.persistence.channel.ChannelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,9 +13,19 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ChannelValidator {
     private final ChannelRepository channelRepository;
+    private final UserChannelRepository userChannelRepository;
 
     public void validateExistChannel(final Long channelId) {
         channelRepository.findById(channelId)
                 .orElseThrow(() -> new ChannelValidationException(ExceptionCode.CHANNEL_NOT_FOUND));
     }
+
+    public void checkChannelParticipation(final User loginUser, final Long channelId) {
+        Channel channel = channelRepository.findById(channelId)
+                .orElseThrow(() -> new ChannelValidationException(ExceptionCode.CHANNEL_NOT_FOUND));
+
+        userChannelRepository.findByUserAndChannel(loginUser, channel)
+                .orElseThrow(() -> new ChannelValidationException(ExceptionCode.USER_NOT_IN_CHANNEL));
+    }
+
 }
