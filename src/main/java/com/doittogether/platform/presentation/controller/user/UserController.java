3dfb49746b88ef3efcc97fc9adef0198a5615168
Yechan Jung <skfk286@hanmail.net>
2024-12-04 14:16:27 +0900

    feat : 컨트롤러 앤드포인트 추가

---
+++ src/main/java/com/doittogether/platform/presentation/controller/user/UserController.java
@@ -0,0 +1,94 @@
+package com.doittogether.platform.presentation.controller.user;
+
+import com.doittogether.platform.application.global.code.SuccessCode;
+import com.doittogether.platform.application.global.response.SuccessResponse;
+import com.doittogether.platform.business.user.UserService;
+import com.doittogether.platform.domain.entity.User;
+import com.doittogether.platform.presentation.dto.user.request.UserUpdateRequest;
+import com.doittogether.platform.presentation.dto.user.response.UserResponse;
+import com.doittogether.platform.presentation.dto.user.response.UserUpdateResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@Tag(name = "회원 API", description = "회원 관리 API")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/my")
+    @Operation(summary = "나의 회원 정보 조회", description = "나의 회원 정보를 조회합니다.")
+    public ResponseEntity<SuccessResponse<UserResponse>> getCurrentUser(Principal principal) {
+        Long userId = Long.parseLong(principal.getName());
+        User user = userService.findByIdOrThrow(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                SuccessResponse.onSuccess(
+                        SuccessCode._OK,
+                        UserResponse.from(user)
+                ));
+    }
+
+    @PatchMapping("/my/profile")
+    @Operation(summary = "나의 프로필 수정", description = "나의 프로필 정보를 수정합니다.")
+    public ResponseEntity<SuccessResponse<UserUpdateResponse>> updateNickname(
+            Principal principal,
+            @RequestBody @Valid UserUpdateRequest userUpdateRequest) {
+        Long userId = Long.parseLong(principal.getName());
+        User user = userService.findByIdOrThrow(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                SuccessResponse.onSuccess(
+                        SuccessCode._OK,
+                        userService.updateNickname(user, userUpdateRequest)
+                ));
+    }
+
+    @GetMapping("/{userId}")
+    @Operation(summary = "특정 사용자 정보 조회", description = "특정 사용자의 정보를 조회합니다.")
+    public ResponseEntity<SuccessResponse<UserResponse>> getUserById(@PathVariable("userId") Long userId) {
+        User user = userService.findByIdOrThrow(userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                SuccessResponse.onSuccess(
+                        SuccessCode._OK,
+                        UserResponse.from(user)
+                )
+        );
+    }
+
+    @GetMapping("/my/profile/complete")
+    @Operation(summary = "프로필 설정 완료 여부 조회", description = "사용자의 프로필 설정 완료 여부를 조회합니다.")
+    public ResponseEntity<SuccessResponse<Boolean>> isProfileComplete(Principal principal) {
+        Long userId = Long.parseLong(principal.getName());
+        User user = userService.findByIdOrThrow(userId);
+
+        boolean isComplete = userService.isProfileComplete(user);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                SuccessResponse.onSuccess(SuccessCode._OK, isComplete)
+        );
+    }
+
+    @PatchMapping("/my/profile/complete")
+    @Operation(summary = "프로필 상태 업데이트", description = "사용자의 프로필 상태를 '완료'로 변경합니다.")
+    public ResponseEntity<SuccessResponse<Void>> completeProfile(Principal principal) {
+        Long userId = Long.parseLong(principal.getName());
+        User user = userService.findByIdOrThrow(userId);
+
+        userService.completeProfile(user);
+
+        return ResponseEntity.status(HttpStatus.OK).body(
+                SuccessResponse.onSuccess(SuccessCode._OK)
+        );
+    }
+}
