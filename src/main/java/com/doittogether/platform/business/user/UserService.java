a939020f5e93cc660e2015fe872c65a918647d70
Yechan Jung <skfk286@hanmail.net>
2024-12-04 14:12:28 +0900

    refactor : 유저에서 필요한 서비스 제공 및 앤드포인트 정의

--- src/main/java/com/doittogether/platform/business/user/UserService.java
+++ src/main/java/com/doittogether/platform/business/user/UserService.java
@@ -1,7 +1,15 @@
 package com.doittogether.platform.business.user;
 
 import com.doittogether.platform.domain.entity.User;
+import com.doittogether.platform.presentation.dto.user.request.UserUpdateRequest;
+import com.doittogether.platform.presentation.dto.user.response.UserUpdateResponse;
 
 public interface UserService {
     User findByIdOrThrow(Long id);
+
+    boolean isProfileComplete(User user);
+
+    UserUpdateResponse updateNickname(User user, UserUpdateRequest request);
+
+    void completeProfile(User user);
 }
--- src/main/java/com/doittogether/platform/business/user/UserServiceImpl.java
+++ src/main/java/com/doittogether/platform/business/user/UserServiceImpl.java
@@ -4,8 +4,12 @@ import com.doittogether.platform.application.global.code.ExceptionCode;
 import com.doittogether.platform.application.global.exception.user.UserException;
 import com.doittogether.platform.domain.entity.User;
 import com.doittogether.platform.infrastructure.persistence.UserRepository;
+import com.doittogether.platform.presentation.dto.user.request.UserUpdateRequest;
+import com.doittogether.platform.presentation.dto.user.response.UserResponse;
+import com.doittogether.platform.presentation.dto.user.response.UserUpdateResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,5 +20,26 @@ public class UserServiceImpl implements UserService {
     @Override
     public User findByIdOrThrow(Long id) {
         return userRepository.findById(id).orElseThrow(() -> new UserException(ExceptionCode.USER_NOT_FOUND));
+    }
+
+    @Override
+    public boolean isProfileComplete(User user) {
+        return user.isProfileComplete();
+    }
+
+    @Transactional
+    @Override
+    public UserUpdateResponse updateNickname(User user, UserUpdateRequest request) {
+        user.updateNickName(request.nickName());
+        user = userRepository.save(user);
+
+        return UserUpdateResponse.from(user);
+    }
+
+    @Transactional
+    @Override
+    public void completeProfile(User user) {
+        user.completeProfileSetup();
+        userRepository.save(user);
     }
 }
