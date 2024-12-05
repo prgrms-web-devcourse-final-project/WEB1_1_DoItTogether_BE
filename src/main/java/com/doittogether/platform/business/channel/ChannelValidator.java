c6d8fe6fee4991544b5e02b65060c1e8a84b5a97
Yechan Jung <skfk286@hanmail.net>
2024-12-05 12:58:08 +0900

    refactor : 검증 및 채널 반환 Validate 클래스 수정

--- src/main/java/com/doittogether/platform/business/channel/ChannelValidator.java
+++ src/main/java/com/doittogether/platform/business/channel/ChannelValidator.java
@@ -3,6 +3,7 @@ package com.doittogether.platform.business.channel;
 import com.doittogether.platform.application.global.code.ExceptionCode;
 import com.doittogether.platform.application.global.exception.channel.ChannelValidationException;
 import com.doittogether.platform.application.global.exception.housework.HouseworkException;
+import com.doittogether.platform.domain.entity.Channel;
 import com.doittogether.platform.infrastructure.persistence.channel.ChannelRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,11 @@ public class ChannelValidator {
 
     public void validateExistChannel(final Long channelId) {
         channelRepository.findById(channelId)
+                .orElseThrow(() -> new ChannelValidationException(ExceptionCode.CHANNEL_NOT_FOUND));
+    }
+
+    public Channel validateAndGetChannel(final Long channelId) {
+        return channelRepository.findById(channelId)
                 .orElseThrow(() -> new ChannelValidationException(ExceptionCode.CHANNEL_NOT_FOUND));
     }
 }
