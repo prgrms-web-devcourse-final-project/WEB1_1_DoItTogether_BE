d2f26694df912f5b55c9fca30073a06a5de7caab
eundeang <111877048+eundeang@users.noreply.github.com>
2024-12-06 21:03:08 +0900

    feat: /actuator/health 시큐리티 미적용 패스에 추가

--- src/main/java/com/doittogether/platform/common/config/SecurityConfig.java
+++ src/main/java/com/doittogether/platform/common/config/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
             "/swagger-ui/*",
             "api-docs/swagger-config",
             "/api-docs",
-            "/user/login"
+            "/user/login",
+            "/actuator/health"
     };
 
     @Bean
